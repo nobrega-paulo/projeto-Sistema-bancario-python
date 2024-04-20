0e82f858363fd1cfd562792e4bda4fa10719ad8e
paulo-nobrega <paulo-nobrega.io@gmail.com>
2024-04-20 14:41:19 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -1,9 +1,9 @@
 menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
 
 => """
 
@@ -16,7 +16,7 @@ LIMITE_SAQUES = 3
 while True:
     opcao = input(menu)
 
-    if opcao == "d":
+    if opcao == "1":
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:
@@ -25,7 +25,7 @@ while True:
         else:
              print("Operacão falhou! O valor informado é invalido.")
 
-    elif opcao == "s":
+    elif opcao == "2":
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
@@ -50,13 +50,13 @@ while True:
         else:
             print("Operacão falhou! O valor informado é invalido.")
 
-    elif opcao == "e":
+    elif opcao == "3":
             print("\n ===================EXTRATO===========")
             print("Não foram realizadas movimentações." if not extrato else extrato)
             print(f"\nSaldo: R$ {saldo:.2f}")
             print("========================================")
 
-    elif opcao == "q":
+    elif opcao == "0":
         break
     else:
         print("Operação inválida, por favor selecione a operação novamente.")
