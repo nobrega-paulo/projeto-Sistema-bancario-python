abc97e679a35da37f7a6c69a4aee92432a29ba5e
paulo-nobrega <165218288+nobrega-paulo@users.noreply.github.com>
2024-05-05 14:11:05 -0300

    Update sistema_bancario.py

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -5,7 +5,8 @@ menu = """
 [3] Extrato
 [0] Sair
 
-=> """
+
+"""
 
 saldo = 0
 limite = 500
@@ -17,6 +18,9 @@ while True:
     opcao = int(input(menu))
 
     if opcao == 1:
+        print("""
+
+     """)
         valor = float(input("Informe o valor do depósito: "))
 
         if valor > 0:
@@ -24,8 +28,13 @@ while True:
              extrato += f"Depósito: R$ {valor:.2f}\n"
         else:
              print("Operacão falhou! O valor informado é invalido.")
+             print("""
 
+     """)
     elif opcao == 2:
+        print("""
+
+     """)
         valor = float(input("Informe o valor do saque: "))
 
         excedeu_saldo = valor > saldo
@@ -36,20 +45,28 @@ while True:
 
         if excedeu_saldo:
              print("Operacão falhou! Você não tem saldo suficiente.")
+             print("""
 
+     """)
         elif excedeu_limite:
              print("Operação falhou! O valor do saque excede o limite.")
+             print("""
 
+     """)
         elif excedeu_saques:
              print("Operação falhou! Número máximo de saques excedidos.")
+             print("""
 
+     """)
         elif valor > 0:
              saldo -= valor
              extrato += f"Saque: R$ {valor:.2f}\n"
              numero_saques += 1
         else:
             print("Operacão falhou! O valor informado é invalido.")
+            print("""
 
+     """)
     elif opcao == 3:
           print("\n ===================EXTRATO===========")
           if extrato:
@@ -59,8 +76,13 @@ while True:
 
           print(f"\nSaldo: R$ {saldo:.2f}")
           print("========================================")
+          print("""
 
+     """)
     elif opcao == 0:
         break
     else:
         print("Operação inválida, por favor selecione a operação novamente.")
+        print("""
+
+""")
